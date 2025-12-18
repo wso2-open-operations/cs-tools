2097949c576c8b15e60c2fed7165dd5fb2601c9c
K.Dileepa Thushan Peiris <dileepapeiris5@gmail.com>
2025-12-18 10:38:32 +0530

    Add side nav bar icons for customer portal
    Introduced new React icon components for the side navigation bar: LegalContractsIcon, ProjectDetailsIcon, SecurityCenterIcon, SettingsIcon, and SupportIcon. These icons will be used to visually represent corresponding sections in the customer portal's navigation.

---
+++ apps/customer-portal/webapp/src/assets/icons/side-nav-bar/support-icon.tsx
@@ -0,0 +1,42 @@
+// Copyright (c) 2025 WSO2 LLC. (https://www.wso2.com).
+//
+// WSO2 LLC. licenses this file to you under the Apache License,
+// Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+import React from "react";
+import type { IconProps } from "../../../types/icon.types";
+
+const SupportIcon: React.FC<IconProps> = ({
+  width,
+  height,
+}) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      aria-hidden="true"
+    >
+      <path d="M3 14h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-7a9 9 0 0 1 18 0v7a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2h3"></path>
+    </svg>
+  );
+};
+
+export default SupportIcon;
