bf58ce100edff27fa66a6fce7860a24b7e062075
K.Dileepa Thushan Peiris <dileepapeiris5@gmail.com>
2026-01-28 10:10:21 +0530

    Refactor main entry to use AppWithConfig and add license
    Replaces App and QueryClientProvider with AppWithConfig in the main entry point. Adds Apache 2.0 license header to the file.

--- apps/customer-portal/webapp/src/main.tsx
+++ apps/customer-portal/webapp/src/main.tsx
@@ -1,15 +1,25 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// Copyright (c) 2026 WSO2 LLC. (https://www.wso2.com).
+//
+// WSO2 LLC. licenses this file to you under the Apache License,
+// Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
 
-const queryClient: QueryClient = new QueryClient();
+import React from "react";
+import { createRoot } from "react-dom/client";
+import AppWithConfig from "@/AppWithConfig.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </StrictMode>,
+  <React.StrictMode>
+    <AppWithConfig />
+  </React.StrictMode>,
 );
