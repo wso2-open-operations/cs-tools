a838a967a9d5b429a454073091efc7ce5e14b7eb
Lithika Damnod <l.d.b.dasanayaka@gmail.com>
2026-04-14 22:42:46 +0530

    fix: disable pie chart animations to avoid blocking main render

--- apps/customer-portal/microapp/src/components/features/dashboard/PieChartWidget.tsx
+++ apps/customer-portal/microapp/src/components/features/dashboard/PieChartWidget.tsx
@@ -52,6 +52,7 @@ export function PieChartWidget({ title, data }: PieChartWidgetProps) {
             margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
             legend={{ show: false }}
             tooltip={{ show: false }}
+            isAnimationActive={false}
           />
         )}
 
