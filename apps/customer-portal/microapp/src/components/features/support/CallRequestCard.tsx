cd7b70165eaa83b20dd21253aacdc856d46f2daf
Lithika Damnod <l.d.b.dasanayaka@gmail.com>
2026-04-17 14:58:11 +0530

    chore: enhance readability and visual structure of call request card

--- apps/customer-portal/microapp/src/components/features/support/CallRequestCard.tsx
+++ apps/customer-portal/microapp/src/components/features/support/CallRequestCard.tsx
@@ -1,6 +1,6 @@
 import type { CallRequestDto } from "@root/src/types/engagement.dto";
-import { Card, Chip, colors, pxToRem, Stack, Typography, Box } from "@wso2/oxygen-ui";
-import { Clock, PhoneCall } from "@wso2/oxygen-ui-icons-react";
+import { Card, Chip, colors, pxToRem, Stack, Typography, Box, Divider } from "@wso2/oxygen-ui";
+import { Calendar, Clock, PhoneCall } from "@wso2/oxygen-ui-icons-react";
 
 export function CallRequestCard(props: CallRequestDto) {
   return (
@@ -12,7 +12,7 @@ export function CallRequestCard(props: CallRequestDto) {
       <Stack spacing={1}>
         <Stack direction="row" spacing={2} alignItems="flex-start" justifyContent="space-between">
           <Stack direction="row" spacing={1.5} alignItems="center">
-            <PhoneCall color={colors.blue[600]} size={pxToRem(18)} />
+            <PhoneCall color={colors.blue[600]} size={pxToRem(17)} />
             <Box>
               <Typography variant="body1" fontWeight="medium" color="text.primary">
                 Call Request
@@ -21,22 +21,21 @@ export function CallRequestCard(props: CallRequestDto) {
           </Stack>
           <Chip size="small" label={props.state.label} />
         </Stack>
-
-        <Stack
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-          sx={{
-            pt: 1,
-            borderTop: `1px solid ${colors.grey[100]}`,
-          }}
-        >
-          <Typography variant="subtitle2" color="text.secondary">
-            {props.createdOn}
-          </Typography>
+        <Divider />
+        <Stack direction="row" justifyContent="space-between" alignItems="center">
+          <Stack direction="row" gap={1} alignItems="center">
+            <Box color="text.secondary">
+              <Calendar size={pxToRem(13)} />
+            </Box>
+            <Typography variant="subtitle2" color="text.secondary">
+              {props.createdOn}
+            </Typography>
+          </Stack>
 
           <Stack direction="row" gap={0.5} alignItems="center">
-            <Clock size={pxToRem(13)} color={colors.grey[500]} />
+            <Box color="text.secondary">
+              <Clock size={pxToRem(13)} />
+            </Box>
             <Typography variant="subtitle2" color="text.secondary">
               {props.durationMin} Minutes
             </Typography>
