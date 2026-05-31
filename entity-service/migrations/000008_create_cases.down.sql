dbf88cd73fb420ed00e9cb3b32e9debd41968145
cloby99 <anuradhabsnk@gmail.com>
2026-05-31 23:56:46 +0530

    [Customer Entity] Add cases table migration with integrity triggers and indexes

---
+++ entity-service/migrations/000008_create_cases.down.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS cases;
+DROP TYPE IF EXISTS case_state_enum;
+DROP TYPE IF EXISTS case_priority_enum;
+DROP FUNCTION IF EXISTS check_case_deployment_belongs_to_project();
+DROP FUNCTION IF EXISTS check_case_deployed_product_belongs_to_deployment();
+DROP FUNCTION IF EXISTS check_case_catastrophic_priority();
